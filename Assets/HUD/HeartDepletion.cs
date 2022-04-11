4fd9a0311760f965d0da3df26ea1d2a2322eeb27
theryguy359 <70359019+theryguy359@users.noreply.github.com>
2022-04-10 19:29:48 -0700

    Added UI elements under hudtest scene

---
+++ Assets/HUD/HeartDepletion.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartDepletion : MonoBehaviour
+{
+    //public Image heart;
+
+
+    void Start()
+    {
+        //heart.enabled = true;
+    }
+
+}
