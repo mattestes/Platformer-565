b85b47cb27df4ba103d82e4a0fa4560785801676
theryguy359 <70359019+theryguy359@users.noreply.github.com>
2022-04-11 14:45:28 -0700

    Added Logic to the Heart HUD so that the hearts disappear when the the left mouse button is clicked

--- Assets/HUD/HeartDepletion.cs
+++ Assets/HUD/HeartDepletion.cs
@@ -1,15 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeartDepletion : MonoBehaviour
 {
-    //public Image heart;
+    bool heart1;
+    bool heart2;
 
 
     void Start()
     {
-        //heart.enabled = true;
+        heart1 = true;
+        heart2 = true;
+        GameObject heart = GameObject.FindGameObjectWithTag("Heart1");
+        Image image = heart.GetComponent<Image>();
+        image.enabled = true;
+        heart = GameObject.FindGameObjectWithTag("Heart2");
+        image = heart.GetComponent<Image>();
+        image.enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (heart1)
+            {
+                GameObject heart = GameObject.FindGameObjectWithTag("Heart1");
+                Image image = heart.GetComponent<Image>();
+                image.enabled = false;
+                heart1 = false;
+            }
+            else
+            {
+                GameObject heart = GameObject.FindGameObjectWithTag("Heart2");
+                Image image = heart.GetComponent<Image>();
+                image.enabled = false;
+                heart2 = false;
+            }
+        }
     }
 
 }
